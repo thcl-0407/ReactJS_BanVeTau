3922ac2b3b4ffadd97a21a4215df28d51884f205
dinhthong173 <dinhthong0123@gmail.com>
2021-05-11 13:24:16 +0700

    done function getAllLichTrinh

--- src/modules/Admin/pages/QuanLyLichTrinh/QuanLyLichTrinh.js
+++ src/modules/Admin/pages/QuanLyLichTrinh/QuanLyLichTrinh.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import AdminService from "../../../../services/Admin.Service"
 
-function QuanLyLichTrinh(){
+function QuanLyLichTrinh(props){
     const [data, setData]= useState([])
     //let gaDen= document.getElementById("txtGaDen").value;
     useEffect(()=>{
@@ -13,6 +13,9 @@ function QuanLyLichTrinh(){
     return(
         <div className="flex justify-center pt-8">
             <div>
+            <div>
+              <button className="bg-blue-300 text-white py-1 px-3 rounded-sm text-xl mb-2 ml-10">Thêm mới lịch trình</button>
+            </div>
             <table className="border-collapse border border-green-800 shadow-lg bg-white ml-10">
           <thead className="table-header-group">
             <tr className="bg-blue-100 border text-left px-8 py-4">
@@ -20,16 +23,19 @@ function QuanLyLichTrinh(){
                 STT
               </th>
               <th className="w-1/8 bg-blue-100 border text-left px-8 py-4">
-                Mã lịch trình
+                Tên Tàu
               </th>
               <th className="w-1/8 bg-blue-100 border text-left px-8 py-4">
-                Ga đi
+                Tên Ga đi
               </th>
               <th className="w-1/8 bg-blue-100 border text-left px-8 py-4" id="txtGaDen">
-                Ga đến
+                Tên Ga đến
               </th>
               <th className="w-1/8 bg-blue-100 border text-left px-8 py-4">
                   Thời gian đi
+              </th>
+              <th className="w-1/8 bg-blue-100 border text-left px-8 py-4">
+                  Thời gian đến
               </th>
               <th className="w-1/8 bg-blue-100 border text-left px-8 py-4">
                 Hoạt động
@@ -44,11 +50,11 @@ function QuanLyLichTrinh(){
                   className="bg-white border px-8 py-4 text-center"
                     >
                   <td className="px-2 py-2">{index + 1}</td>
-                  <td>{item.MaLichTrinh}</td>
-                  <td>{item.TenNhaGa}</td>
-                  <td>{item.TenNhaGa}</td>
+                  <td>{item.TenTau}</td>
+                  <td>{item.TenGaDi}</td>
+                  <td>{item.TenGaDen}</td>
                   <td>{item.ThoiGianDi}</td>
-                  
+                  <td>{item.ThoiGianDen}</td>
                   <td>
                     {/* <button
                       className="underline text-blue-400"
