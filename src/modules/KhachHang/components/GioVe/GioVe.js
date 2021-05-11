87e9dc45c027718e9ea81a3c8bd071d96fc1cecd
thcl-0407 <thcloc@icloud.com>
2021-05-11 20:17:52 +0700

    Check Click MuaVe When User don't login and TicketCart hasn't no one

--- src/modules/KhachHang/components/GioVe/GioVe.js
+++ src/modules/KhachHang/components/GioVe/GioVe.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import lodash from "lodash";
 import ToastifyMessage from "./../../../../utilities/ToastifyMessage"
+import { reactLocalStorage } from 'reactjs-localstorage';
+import history from "./../../../../history"
 
 function GioVe(props) {
-    function btnMuaVe_Click(){
-        
+    function btnMuaVe_Click() {
+        //Check Đăng Nhập
+        if (lodash.isEmpty(reactLocalStorage.getObject('CurrentUser'))) {
+            ToastifyMessage.ToastError("Bạn Cần Đăng Nhập Để Thực Hiện Hành Động Này")
+            return;
+        }
+
+        //Check Chưa Thêm Vé Vào Giỏ Vé
+        if(lodash.isUndefined(props.DSVe)){
+            ToastifyMessage.ToastError("Không Có Vé Trong Giỏ")
+            return;
+        }
     }
 
     return (
@@ -17,7 +29,7 @@ function GioVe(props) {
                     <strong className="text-red-500">Chưa Có Vé</strong>
                 </div>
                 <div className="text-center mb-2">
-                    <input type="button" onClick={btnMuaVe_Click} className="cursor-pointer px-2 py-1 border-2 border-main bg-main text-white hover:bg-white hover:text-main hover:pointer" value="Mua Vé"/>
+                    <input type="button" onClick={btnMuaVe_Click} className="cursor-pointer px-2 py-1 border-2 border-main bg-main text-white hover:bg-white hover:text-main hover:pointer" value="Mua Vé" />
                 </div>
             </div>
         </div>
